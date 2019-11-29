5632c2a44d9680ed7a1555f9a146b5f1aedad236
hugoiman <hugogiman17@gmail.com>
2019-11-30 06:08:54 +0700

    release v1

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+)
+
+func main() {
+	http.Handle("/assets/",
+		http.StripPrefix("/assets/",
+			http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/", Index)
+	fmt.Println("server started at localhost:5000")
+	http.ListenAndServe(":5000", nil)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	filepath := path.Join("views", "index.html")
+	tmpl, err := template.ParseFiles(filepath)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
